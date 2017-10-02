143ab6ead369e108c8f5629caacffd6c8f7ead9c
kmuzalewska <klara.muzalewska@gmail.com>
2017-10-02 22:17:26 +0200

    first

---
+++ zad1/zad1.sh
@@ -0,0 +1,48 @@
+#!/bin/bash
+#Klara Muzalewska
+
+USER=$(id -u -n)
+
+helpf() { 
+			echo
+			echo 'Program is created by Klara Muzalewska to display the login, name and surname of the current user.'
+			echo
+			echo 'Possible parameter:'
+			echo
+			echo '	./[FILENAME] 			Displays the login, name and surname of the current user'
+			echo '	./[FILENAME] -h 	--help	Displays the possible options of the program'
+			echo '	./[FILENAME] -q 	--quit	Ends the program' ; }
+
+for i in $@
+do
+	if [[ $i == "--help" ]] || [[ $i == "-h" ]]; then
+		helpf
+		exit 0
+	elif [[ $i == "--quit" ]] || [[ $i == "-q" ]]; then
+		q=1 && continue
+	elif [[ $i == "-"* ]] || [[ $i == "--"* ]]; then
+		v=1 && continue
+	fi
+done
+
+if [[ $q == 1 ]]; then
+	exit 0
+elif [[ $v == 1 ]]; then
+	echo $i, wrong parameter, see: 
+	helpf
+	exit 1
+else
+	echo Do You have installed program named finger \(yes\/no\)
+	read var
+	if [[ $var == "yes" ]]; then
+		DETAILS=$(finger $USER | grep Name:)
+		echo $DETAILS
+	elif [[ $var == "no" ]]; then
+		sudo apt-get install finger
+		DETAILS=$(finger $USER | grep Name:)
+		echo $DETAILS
+	else
+		echo Wrong answer. See that. #that function
+	fi
+fi
+	
