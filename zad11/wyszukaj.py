07957777c826e7c4c83c0e5af36a4c7cb96ddc19
kmuzalewska <klara.muzalewska@gmail.com>
2018-01-08 15:28:34 +0100

    something

--- zad11/wyszukaj.py
+++ zad11/wyszukaj.py
@@ -4,3 +4,29 @@
 
 #Klara Muzalewska
 
+import re
+import string
+import os
+
+def findPhrase(phrase, folder):
+    for root, directories, filenames in os.walk(folder):
+        # print folder
+        for filename in filenames:
+            frequency = {}
+            f=os.path.join(root,filename)  
+            document_text = open(f, 'r')
+            text_string = document_text.read()
+            pattern = re.compile(phrase)
+            match_pattern = re.findall(pattern, text_string)
+            for word in match_pattern:
+                count = frequency.get(word,0)
+                frequency[word] = count + 1
+            frequency_list = frequency.keys()        
+            for words in frequency_list:
+                print f, words, frequency[words]
+
+
+
+
+
+findPhrase('Klara', '/home/klara/Studia/IIst/Isemestr/skrypty/')
