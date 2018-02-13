620a60fc6f7ceea1f4d408b7467175fa3e529b2b
kmuzalewska <klara.muzalewska@gmail.com>
2018-02-13 02:15:40 +0100

    chyba dziala

--- projekty/perl/KlaraMuzalewskaPerlProject.pl
+++ projekty/perl/KlaraMuzalewskaPerlProject.pl
@@ -9,25 +9,47 @@ $nrOfArguments = $#ARGV+1;
 
 sub help(){
 	print "\nProgram napisany przez Klare Muzalewska.\n";
-	print "Program sluzy do tworzenia pliku tex na podstawi istniejacego pliku txt.\n";
+	print "Program sluzy do tworzenia pliku pdf na podstawi istniejacego pliku txt za pomoca oprogramowania latex przy użyciu typu dokumentu: ksiazka.\n";
+	print "Jest mozliwosc bardzo podstawowego formatowania. Opcje sa podawane co paragraf. \n";
+	print "Aby program dzialal poprawnie powinien byc zainstalowany podstawowy pakiet latex.\n";
 }
 
 
-sub interactionWithUser(){
+sub interactionWithBookUser(){
 	local($promptString,$defaultValue) = @_;
 
-	if ($defaultValue) {
-	      print $promptString, "[", $defaultValue, "]: ";
-	   } else {
-	      print $promptString, ": ";
-	   }
+    print "Enter separator: [ $defaultValue ]: \n";
+    print "a) \\\\ \n";
+    print 'b) \chapter{}';
+    print "\n";
+    print 'c)\begin{center}*****\end{center}';
+    print "\n";
+    print 'q) Create pdf with texts from origin text but no more formating.';
+    print "\n";
 
-	   $| = 1;               # force a flush after our print
-	   $_ = <STDIN>;         # get the input from STDIN (presumably the keyboard)
+   	$| = 1;               # force a flush after our print
+   	$_ = <STDIN>;         # get the input from STDIN (presumably the keyboard)
 
-	   chomp;
+	chomp;
+    return $_ ? $_ : $defaultValue;    # return $_ if it has a value
 
+}
+sub interactionWithBeginningkUser() {
+	local($promptString,$defaultValue) = @_;
 
+    print "Enter separator: [ $defaultValue ]: \n";
+    print 'a) \chapter{}';
+    print "\n";
+    print 'b) \begin{center}*****\end{center}' ;
+    print "\n";
+    print 'q) Create pdf with texts from origin text but no more formating.';
+    print "\n";
+
+   	$| = 1;               # force a flush after our print
+   	$_ = <STDIN>;         # get the input from STDIN (presumably the keyboard)
+
+	chomp;
+    return $_ ? $_ : $defaultValue;    # return $_ if it has a value
 }
 
 my @files;
@@ -63,15 +85,99 @@ for (my $var = 0; $var < ($#files+1); $var++) {
     		warn "\nUnable to create $file\n";
     		continue;
 		}
+		# while (1) {
+		# 	print "What type of document: \n a) book \n b) article\n";
+		# 	$type = <STDIN>;
+		# 	if ($type == 'a'){
+		# 		$header="book.txt";
+		# 		last;
+		# 	} elsif ($type == 'b'){
+		# 		$header="article.txt";
+		# 		last;
+		# 	} else{
+		# 		print "Wrong option.";
+		# 	}	
+		# }
+		$header="book.txt";
+		if (open(my $head, '<:encoding(UTF-8)', $header)) {
+			while (my $row = <$head>) {
+				undef @words;
+		        chomp $row;
+		        print FILE "$row\n";
+	    	}
+		}
+		print "Author:\n";
+		print FILE '\author{';
+		$author = <STDIN>;
+		print FILE "$author}\n";
+		print "Title:\n";
+		print FILE '\title{';
+		$title = <STDIN>;
+		print FILE "$title}\n";
+		print FILE '\begin{document}';
+		print FILE '\maketitle';
+		print FILE "\n";
+		$quit=0;
+		$begin=1;
+        if($begin){
+        	$begin=0;
+        	while (1) {
+				$option = &interactionWithBeginningkUser("Enter separator:", "a"); #"Enter separator:"
+				if ($option eq 'a'){
+					print FILE '\chapter{';
+					print "Chapter's title:";
+					$chapter = <STDIN>;
+					print FILE "$chapter}\n";
+					last;
+				} elsif ($option eq 'b'){
+					print FILE '\begin{center}*****\end{center}';
+					last;
+				}elsif ($option eq 'q'){
+					$quit=1;
+					last;
+				} else{
+					print "\nWrong option.\n";
+				}	
+			}
+        }
 		while (my $row = <$fh>) {
 			undef @words;
 	        chomp $row;
-	        print FILE "$row\n";
-	        # print "$row\n";	        	      
-	        # print "\n";
-	    }
+	        if ($quit) {
+	        	print FILE "$row\n";
+	        } else{
+		        print "Paragraph:\n";
+	    		print $row;
+	    		print "\n";
+	    		print FILE "$row\n";
+	    		while (1) {
+					$option = &interactionWithBookUser("Enter separator:", "a"); #"Enter separator:"
+					if ($option eq 'a'){
+						print FILE "\\\\";
+						last;
+					} elsif ($option eq 'b'){
+						print FILE '\chapter{';
+						print "Chapter's title:";
+						$chapter = <STDIN>;
+						print FILE "$chapter}\n";
+						last;
+					} elsif ($option eq 'c'){
+						print FILE '\begin{center}*****\end{center}';
+						last;
+					}elsif ($option eq 'q'){
+						$quit=1;
+						last;
+					} else{
+						print "\nWrong option.\n";
+					}	
+				}
+		        print "\n";
+		    }
+		}
+	    print FILE '\end{document}';
 	    close FILE;
 	} else {
 	    warn "Could not open file '$filename' $!\n";
 	}
-}
+	exec("pdflatex $fileToWrite");
+}print FILE '\begin{center}*****\end{center}';
